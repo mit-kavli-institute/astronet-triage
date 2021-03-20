Minor fixes to the calculation of n_points and n_folds. Additional filtering of individual views.

astronet/preprocess/preprocess.py
@@ -80,7 +80,8 @@ def generate_view(tic_id,
                   t_min,
                   t_max,
                   normalize=True,
-                  binning=None
+                  binning=None,
+                  trim_edges=False,
                  ):
   """Generates a view of a phase-folded light curve using a median filter.
 
@@ -97,10 +98,10 @@ def generate_view(tic_id,
     uniformly spaced bins on the phase-folded time axis.
   """
   if binning is None:
-    view, mask, std = median_filter2.new_binning(time, flux, period, num_bins, t_min, t_max)
+    view, mask, std = median_filter2.new_binning(time, flux, period, num_bins, t_min, t_max, trim_edges=trim_edges)
   else:
     view, mask, std = median_filter2.new_binning(
-        time, flux, period, num_bins, t_min, t_max, method=binning)
+        time, flux, period, num_bins, t_min, t_max, method=binning, trim_edges=trim_edges)
 
   scale = 0.0
   if normalize:
@@ -330,10 +331,12 @@ def sample_segments_view(tic_id,
                          flux,
                          fold_num,
                          period,
-                         num_bins=1000,
+                         duration,
+                         num_bins=201,
                          num_transits=7):
     times, fluxes, nums = sample_segments(time, flux, fold_num, period, num_transits=num_transits)
     full_view = []
+    transit_view = []
     for t, f, n in zip(times, fluxes, nums):
         view, _, mask, _ = generate_view(
                 tic_id, 
@@ -344,9 +347,11 @@ def sample_segments_view(tic_id,
                 t_min=(period * (n - 0.5)),
                 t_max=(period * (n + 0.5)),
                 normalize=False,
+                trim_edges=True,
             )
         full_view.append(view)
         full_view.append(mask)
+
     for _ in range(num_transits - len(times)):
         full_view.append(np.zeros([num_bins], dtype=float))
         full_view.append(np.zeros([num_bins], dtype=float))

light_curve_util/keplersplinev2.py
@@ -390,11 +390,13 @@ def choosekeplersplinev2(time,flux, bkspace_min=0.5, bkspace_max=20, bkspace_num
                          return_metadata=False, fixed_bkspace=None):
     if gap_width_in == None:
         gap_width_in = bkspace_min
+        if fixed_bkspace is not None:
+            gap_width_in = min(gap_width_in, fixed_bkspace)
     if np.all(input_mask == None):
         input_mask = np.ones(len(time), dtype=bool)
 
     all_time, all_flux = split(time, flux, gap_width=gap_width_in) 
-    all_time2, all_input_mask = split(time, input_mask, gap_width=gap_width_in) 
+    all_time2, all_input_mask = split(time, input_mask, gap_width=gap_width_in)
     
     if fixed_bkspace:
         bkspaces = [fixed_bkspace]

light_curve_util/median_filter2.py
@@ -34,7 +34,7 @@ def get_overlap(hbw, t, c):
     return bin_overlap
 
 
-def new_binning(time, flux, period, num_bins, t_min, t_max, method='weighted_mean'):
+def new_binning(time, flux, period, num_bins, t_min, t_max, method='weighted_mean', trim_edges=False):
   t = time.copy()
   
   bins_left_edge, step = np.linspace(
@@ -93,5 +93,16 @@ def new_binning(time, flux, period, num_bins, t_min, t_max, method='weighted_mea
         f[i] = np.max(f_x)
 
     s[i] = np.std(f_x)
+    
+  if trim_edges:
+      clear_bins = set()
+      for i in range(len(m)):
+        if m[i] < 1:
+            if i > 0:
+                clear_bins.add(i - 1)
+            if i < len(m) - 1:
+                clear_bins.add(i + 1)
+      for i in list(clear_bins):
+        m[i] = 0.0
 
   return f, m, s