1adf6c6fa1fffbf34ac3fe9a3d76f470c50b0a87
Dan Moldovan <mdan@google.com>
2021-08-29 19:14:26 +0000

    Fix script.

--- astronet/gen_tfrecords_vetting.sh
+++ astronet/gen_tfrecords_vetting.sh
@@ -5,10 +5,10 @@ set -e
 TEMPDIR=tmp-vetting/tfrecords
 LCDIR=/mnt/tess/lc-v
 
-# rm -Rf ${TEMPDIR}
-# mkdir -p ${TEMPDIR}
+rm -Rf ${TEMPDIR}
+mkdir -p ${TEMPDIR}
 
-# python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-vetting-v1-train.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-vetting-1-train --vetting_features=y --num_shards=2
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-vetting-v1-train.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-vetting-1-train --vetting_features=y --num_shards=2
 
 python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-vetting-v1-val.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-vetting-1-val --vetting_features=y --num_shards=2
 
