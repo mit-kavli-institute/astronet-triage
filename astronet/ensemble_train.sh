c3e82efa3281597de677c00ab20c6bb15a8a397e
Dan Moldovan <mdan@google.com>
2021-08-29 14:01:11 +0000

    Update scripts.

--- astronet/ensemble_train.sh
+++ astronet/ensemble_train.sh
@@ -8,10 +8,10 @@ do
     python astronet/train.py \
         --model=AstroCNNModel \
         --config_name=extended \
-        --train_files='/mnt/tess/astronet/tfrecords-25-train/*' \
-        --eval_files='/mnt/tess/astronet/tfrecords-25-val/*' \
+        --train_files='/mnt/tess/astronet/tfrecords-26-train/*' \
+        --eval_files='/mnt/tess/astronet/tfrecords-26-val/*' \
         --train_steps=20000 \
         --train_epochs=1 \
-        --model_dir="/mnt/tess/astronet/checkpoints/extended_25_run_17/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/extended_26_run_18/${i}"
 done
 
