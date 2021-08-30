50cb9c203d46b0f86490421a2dafc3aca9927314
Dan Moldovan <mdan@google.com>
2021-08-30 14:54:19 +0000

    Fix bug.

--- astronet/ensemble_train_vetting.sh
+++ astronet/ensemble_train_vetting.sh
@@ -10,8 +10,8 @@ do
         --config_name=base \
         --train_files='/mnt/tess/astronet/tfrecords-vetting-1-train/*' \
         --eval_files='/mnt/tess/astronet/tfrecords-vetting-1-val/*' \
-        --pretrain_model_dir="/mnt/tess/astronet/checkpoints/extended_26_run_18/${i}"
-        --train_steps=20000 \
+        --pretrain_model_dir="/mnt/tess/astronet/checkpoints/extended_26_run_18/${i}" \
+        --train_steps=5000 \
         --train_epochs=1 \
         --model_dir="/mnt/tess/astronet/checkpoints/vetting_base_1_run_1/${i}"
 done
