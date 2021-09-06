072ca4e486c74a3a4d38a0dab9874d2416407a13
Dan Moldovan <mdan@google.com>
2021-09-06 14:50:54 +0000

    New run.

--- astronet/ensemble_train_vetting.sh
+++ astronet/ensemble_train_vetting.sh
@@ -8,11 +8,11 @@ do
     python astronet/train.py \
         --model=AstroCNNModelVetting \
         --config_name=base \
-        --train_files='/mnt/tess/astronet/tfrecords-vetting-1-train/*' \
-        --eval_files='/mnt/tess/astronet/tfrecords-vetting-1-val/*' \
+        --train_files='/mnt/tess/astronet/tfrecords-vetting-2-train/*' \
+        --eval_files='/mnt/tess/astronet/tfrecords-vetting-2-val/*' \
         --pretrain_model_dir="/mnt/tess/astronet/checkpoints/extended_26_run_18/${i}" \
-        --train_steps=20000 \
+        --train_steps=6000 \
         --train_epochs=1 \
-        --model_dir="/mnt/tess/astronet/checkpoints/vetting_base_1_run_3/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/vetting_base_2_run_4/${i}"
 done
 
