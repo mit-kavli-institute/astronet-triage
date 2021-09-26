1a284db49905d4398225bbef8d122f4ab8631689
Dan Moldovan <mdan@google.com>
2021-09-26 19:39:51 +0000

    Another run whic doesnt unclude unrevised data.

--- astronet/gen_tfrecords.sh
+++ astronet/gen_tfrecords.sh
@@ -8,9 +8,9 @@ LCDIR=/mnt/tess/lc
 rm -Rf ${TEMPDIR}
 mkdir -p ${TEMPDIR}
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v7-train.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-26-train --num_shards=20
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v8-train.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-27-train --num_shards=20
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v7-val.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-26-val --num_shards=2
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v8-val.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-27-val --num_shards=2
 
 # python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v5-test.csv --tess_data_dir=${LCDIR} --output_dir=${TEMPDIR}/tfrecords-25-test --num_shards=2
 
