0f2c220355b4b7a33e5bd7d4583f6a9a3cf0eda4
Dan Moldovan <mdan@google.com>
2021-10-15 13:47:48 +0000

    Add test config.

--- astronet/ensemble_train_vetting.sh
+++ astronet/ensemble_train_vetting.sh
@@ -2,31 +2,45 @@
 
 set -e
 
+# for i in {1..10}
+# do
+#     echo "Training model ${i}"
+#     python astronet/train.py \
+#         --model=AstroCNNModelVetting \
+#         --config_name=vrevised_tuned \
+#         --train_files='/mnt/tess/astronet/tfrecords-vetting-5-toi-train/*' \
+#         --eval_files='/mnt/tess/astronet/tfrecords-vetting-5-toi-val/*' \
+#         --pretrain_model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
+#         --train_steps=0 \
+#         --train_epochs=1 \
+#         --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_run_1/${i}"
+# done
+
+# for i in {1..10}
+# do
+#     echo "Training model ${i}"
+#     python astronet/train.py \
+#         --model=AstroCNNModelVetting \
+#         --config_name=vrevised_tuned \
+#         --train_files='/mnt/tess/astronet/tfrecords-vetting-5-train/*' \
+#         --eval_files='/mnt/tess/astronet/tfrecords-vetting-5-val/*' \
+#         --pretrain_model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
+#         --train_steps=0 \
+#         --train_epochs=1 \
+#         --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_notoi_run_1/${i}"
+# done
+
+rm -Rf /mnt/tess/astronet/checkpoints/temp_test
 for i in {1..10}
 do
     echo "Training model ${i}"
     python astronet/train.py \
         --model=AstroCNNModelVetting \
-        --config_name=vrevised_tuned \
+        --config_name=temp_test \
         --train_files='/mnt/tess/astronet/tfrecords-vetting-5-toi-train/*' \
         --eval_files='/mnt/tess/astronet/tfrecords-vetting-5-toi-val/*' \
         --pretrain_model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
         --train_steps=0 \
         --train_epochs=1 \
-        --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_run_1/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/temp_test/${i}"
 done
-
-for i in {1..10}
-do
-    echo "Training model ${i}"
-    python astronet/train.py \
-        --model=AstroCNNModelVetting \
-        --config_name=vrevised_tuned \
-        --train_files='/mnt/tess/astronet/tfrecords-vetting-5-train/*' \
-        --eval_files='/mnt/tess/astronet/tfrecords-vetting-5-val/*' \
-        --pretrain_model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
-        --train_steps=0 \
-        --train_epochs=1 \
-        --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_notoi_run_1/${i}"
-done
-
