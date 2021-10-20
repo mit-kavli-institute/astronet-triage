1c8d358b60ba2fbac21dbb1c2a54676dc9cfddf2
Dan Moldovan <mdan@google.com>
2021-10-20 16:18:44 +0000

    Update configuration.

--- astronet/ensemble_train_vetting.sh
+++ astronet/ensemble_train_vetting.sh
@@ -13,7 +13,7 @@ do
         --pretrain_model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
         --train_steps=0 \
         --train_epochs=1 \
-        --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_run_1/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_run_2/${i}"
 done
 
 for i in {1..10}
@@ -23,9 +23,9 @@ do
         --model=AstroCNNModelVetting \
         --config_name=vrevised_tuned \
         --train_files='/mnt/tess/astronet/tfrecords-vetting-5-train/*' \
-        --eval_files='/mnt/tess/astronet/tfrecords-vetting-5-val/*' \
+        --eval_files='/mnt/tess/astronet/tfrecords-vetting-5-toi-val/*' \
         --pretrain_model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
         --train_steps=0 \
         --train_epochs=1 \
-        --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_notoi_run_1/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/vrevised_tuned_5_notoi_run_2/${i}"
 done
