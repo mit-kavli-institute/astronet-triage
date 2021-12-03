32c8dbbb408c486cf15312a321cf354ea8ca0a98
Dan Moldovan <mdan@google.com>
2021-12-03 13:51:47 +0000

    Updated versions.

--- astronet/gen_tfrecords.sh
+++ astronet/gen_tfrecords.sh
@@ -4,8 +4,8 @@ set -e
 
 LCDIR=/mnt/tess/lc
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v8-train.csv --tess_data_dir=${LCDIR} --output_dir=/mnt/tess/astronet/tfrecords-30-train --num_shards=50
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v9-train.csv --tess_data_dir=${LCDIR} --output_dir=/mnt/tess/astronet/tfrecords-31-train --num_shards=50
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v8-val.csv --tess_data_dir=${LCDIR} --output_dir=/mnt/tess/astronet/tfrecords-30-val --num_shards=5
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v9-val.csv --tess_data_dir=${LCDIR} --output_dir=/mnt/tess/astronet/tfrecords-31-val --num_shards=5
 
-# python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v5-test.csv --tess_data_dir=${LCDIR} --output_dir=/mnt/tess/astronet/tfrecords-30-test --num_shards=5
+# python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/mnt/tess/astronet/tces-v9-test.csv --tess_data_dir=${LCDIR} --output_dir=/mnt/tess/astronet/tfrecords-31-test --num_shards=5
