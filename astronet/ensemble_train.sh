b4ee310344bab10e0cc273cd7367ab13b9f3f5c4
Dan Moldovan <mdan@google.com>
2022-02-17 23:13:48 +0000

    New run with more instar exclusions.

--- astronet/ensemble_train.sh
+++ astronet/ensemble_train.sh
@@ -7,10 +7,10 @@ do
     echo "Training model ${i}"
     python astronet/train.py \
         --model=AstroCNNModel \
-        --config_name=revised_tuned \
-        --train_files='/mnt/tess/astronet/tfrecords-34-train/*' \
-        --eval_files='/mnt/tess/astronet/tfrecords-34-val/*' \
+        --config_name=revised \
+        --train_files='/mnt/tess/astronet/tfrecords-35-train/*' \
+        --eval_files='/mnt/tess/astronet/tfrecords-35-val/*' \
         --train_steps=0 \
-        --model_dir="/mnt/tess/astronet/checkpoints/revised_tuned_34_run_1/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/revised_35_run_1/${i}"
 done
 
