fef4ff3ac9d59f4732ec51d22bbe350d35548d0a
Dan Moldovan <mdan@google.com>
2022-03-26 18:08:30 +0000

    Update notebooks.

--- astronet/ensemble_train.sh
+++ astronet/ensemble_train.sh
@@ -7,10 +7,10 @@ do
     echo "Training model ${i}"
     python astronet/train.py \
         --model=AstroCNNModel \
-        --config_name=revised \
-        --train_files='/mnt/tess/astronet/tfrecords-35-train/*' \
-        --eval_files='/mnt/tess/astronet/tfrecords-35-val/*' \
+        --config_name=extended \
+        --train_files='/mnt/tess/astronet/tfrecords-35-y2-train/*' \
+        --eval_files='/mnt/tess/astronet/tfrecords-35-y2-val/*' \
         --train_steps=0 \
-        --model_dir="/mnt/tess/astronet/checkpoints/revised_35_run_1/${i}"
+        --model_dir="/mnt/tess/astronet/checkpoints/ext_35-y2_run_1/${i}"
 done
 
