Fix bug in missing value detection, simplify scaling, include star_rad_est.

astronet/astro_cnn_model/input_ds.py
@@ -72,26 +72,15 @@ def build_dataset(file_pattern,
         for name, value in parsed_features.items():
             cfg = input_config.features[name]
             if not cfg.is_time_series:
-                use_value = True
-                if cfg.has_nans:
-                    if tf.math.is_nan(value):
-                        use_value = False
-                        mask = tf.zeros_like(value)
-                    else:
-                        mask = tf.ones_like(value)
-                    features[f"{name.lower()}_present"] = mask
-                if use_value:
-                    if getattr(cfg, "log_scale", False):
-                        value = tf.cast(value, tf.float64)
-                        value = tf.maximum(value, cfg.min_val)
-                        value = tf.minimum(value, cfg.max_val)
-                        value = value - cfg.min_val + 1
-                        value = tf.math.log(value) / tf.math.log(tf.constant(cfg.max_val, tf.float64))
-                        value = tf.cast(value, tf.float32)
-                    else:
-                        value = (value - cfg["mean"]) / cfg["std"]
-                else:
-                    value = tf.zeros_like(value)
+                if getattr(cfg, "scale", None) == "log":
+                    value = tf.cast(value, tf.float64)
+                    value = tf.maximum(value, cfg.min_val)
+                    value = tf.minimum(value, cfg.max_val)
+                    value = value - cfg.min_val + 1
+                    value = tf.math.log(value) / tf.math.log(tf.constant(cfg.max_val, tf.float64))
+                    value = tf.cast(value, tf.float32)
+                elif getattr(cfg, "scale", None) == "norm":
+                    value = (value - cfg["mean"]) / cfg["std"]
             features[name.lower()] = value
         
         if include_labels:

astronet/preprocess/generate_input_records.py
@@ -59,7 +59,7 @@ def _set_float_feature(ex, name, value):
     value = value.reshape((-1,))
   values = [float(v) for v in value]
   if any(np.isnan(values)):
-    raise ValueError(f'NaNs in {name} for {tic_id}')
+    raise ValueError(f'NaNs in {name}')
   ex.features.feature[name].float_list.value.extend(values)
 
 
@@ -204,17 +204,24 @@ def _process_tce(tce, bkspace=None):
 
   if np.isnan(tce.SMass):
     _set_float_feature(ex, 'star_mass', [0])
-    _set_float_feature(ex, 'star_mass_poresent', [0])
+    _set_float_feature(ex, 'star_mass_present', [0])
   else:
     _set_float_feature(ex, 'star_mass', [tce.SMass])
-    _set_float_feature(ex, 'star_mass_poresent', [1])
+    _set_float_feature(ex, 'star_mass_present', [1])
 
   if np.isnan(tce.SRad):
     _set_float_feature(ex, 'star_rad', [0])
-    _set_float_feature(ex, 'star_rad_poresent', [0])
+    _set_float_feature(ex, 'star_rad_present', [0])
   else:
     _set_float_feature(ex, 'star_rad', [tce.SRad])
-    _set_float_feature(ex, 'star_rad_poresent', [1])
+    _set_float_feature(ex, 'star_rad_present', [1])
+
+  if np.isnan(tce.SRadEst):
+    _set_float_feature(ex, 'star_rad_est', [0])
+    _set_float_feature(ex, 'star_rad_est_present', [0])
+  else:
+    _set_float_feature(ex, 'star_rad_est', [tce.SRadEst])
+    _set_float_feature(ex, 'star_rad_est_present', [1])
 
   _set_float_feature(ex, 'n_folds', [len(set(fold_num))])
   _set_float_feature(ex, 'n_points', [len(fold_num)])