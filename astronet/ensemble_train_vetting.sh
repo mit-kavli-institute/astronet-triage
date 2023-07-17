33344d7440834eb3a083021789d4d1f9639d81dd
Daniel Muthukrishna <daniel.muthukrishna@gmail.com>
2023-07-17 12:35:34 -0400

    Added final triage model to vetting configurations_vetting.py and ensemble_train_vetting.sh

--- astronet/ensemble_train_vetting.sh
+++ astronet/ensemble_train_vetting.sh
@@ -7,10 +7,10 @@ do
     echo "Training model ${i}"
     python astronet/train.py \
         --model=AstroCNNModelVetting \
-        --config_name=direct \
+        --config_name=final_alpha_1_tuned \
         --train_files='../mnt/tess/astronet/tfrecords-vetting-01-train/*' \
         --eval_files='../mnt/tess/astronet/tfrecords-vetting-01-val/*' \
-        --pretrain_model_dir="None" #../mnt/tess/astronet/checkpoints/revised_tuned_30_run_1/${i}" \
+        --pretrain_model_dir='/pdo/users/dmuth/mnt/tess/fa1t_38_run_1/10' \
         --train_steps=0 \
         --train_epochs=1 \
         --model_dir="../mnt/tess/astronet/checkpoints/new_test/${i}" #direct_7_run_4/${i}"
