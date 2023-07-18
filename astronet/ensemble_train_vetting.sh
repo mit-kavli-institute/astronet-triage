1ce0c8980a3143b9ec1916c06de8873609f5489b
Daniel Muthukrishna <daniel.muthukrishna@gmail.com>
2023-07-18 14:07:08 -0400

    Made new config file base_new in configurations_vetting.py

--- astronet/ensemble_train_vetting.sh
+++ astronet/ensemble_train_vetting.sh
@@ -7,11 +7,11 @@ do
     echo "Training model ${i}"
     python astronet/train.py \
         --model=AstroCNNModelVetting \
-        --config_name=final_alpha_1_tuned \
+        --config_name=base_new \
         --train_files='../mnt/tess/astronet/tfrecords-vetting-01-train/*' \
         --eval_files='../mnt/tess/astronet/tfrecords-vetting-01-val/*' \
         --pretrain_model_dir='/pdo/users/dmuth/mnt/tess/fa1t_38_run_1/10' \
-        --train_steps=0 \
+        --train_steps=2500 \  # Try hyperparameter tuning between 500 to 2500 with 500 increments (maybe use an ensemble of 2 or 3 instead of 10 networks)
         --train_epochs=1 \
         --model_dir="../mnt/tess/astronet/checkpoints/new_test/${i}" #direct_7_run_4/${i}"
 done
