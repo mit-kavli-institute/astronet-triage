84385c5bbf79ef954188616462d27acef0d05093
Jack Haviland <haviland@mit.edu>
2024-04-08 12:58:57 -0400

    Make batch prediction methods

--- astronet/direct_tensor/predict.py
+++ astronet/direct_tensor/predict.py
@@ -1,7 +1,8 @@
 """Make astronet predictions without creating tf.training.Example objects"""
 
+import json
 from collections import defaultdict
-from itertools import repeat
+from pathlib import Path
 from typing import Literal, Optional, Protocol
 
 import numpy as np
@@ -197,7 +198,7 @@ def prediction_features(
 
 
 def prepare_input(
-    input_config: dict,
+    feature_cfg: dict,
     tce: pd.Series,
     time: np.ndarray,
     flux: np.ndarray,
@@ -205,15 +206,13 @@ def prepare_input(
 ) -> dict:
     tce_features = prediction_features(tce, time, flux, aperture_fluxes)
     tce_features = {
-        name: value
-        for name, value in tce_features.items()
-        if name in input_config["inputs"]["features"]
+        name: value for name, value in tce_features.items() if name in feature_cfg
     }
 
     features = {}
-    assert set(tce_features.keys()) == set(input_config["inputs"]["features"].keys())
+    assert set(tce_features.keys()) == set(feature_cfg.keys())
     for name, value in tce_features.items():
-        cfg = input_config["inputs"]["features"][name]
+        cfg = feature_cfg[name]
         if not cfg["is_time_series"]:
             if cfg.get("scale", None) == "log":
                 value = tf.cast(value, tf.float64)
@@ -231,14 +230,13 @@ def prepare_input(
 
 
 def build_dataset(
-    input_config: dict,
+    feature_cfg: dict,
     tces: pd.DataFrame,
     get_lc: LCGetter,
     mode: Literal["triage", "vetting"],
 ) -> tf.data.Dataset:
     dataset = defaultdict(list)
-    for cfg, (_, tce) in zip(repeat(input_config), tces.iterrows()):
-        print(tce)
+    for _, tce in tces.iterrows():
         time, flux = get_lc(tce["Astro ID"])
         aperture_fluxes: dict[str, tuple[np.ndarray, np.ndarray]] = {}
         if mode == "vetting":
@@ -246,7 +244,67 @@ def build_dataset(
                 aperture: get_lc(tce["Astro ID"], aperture)
                 for aperture in ["s", "m", "l"]
             }
-        features = prepare_input(cfg, tce, time, flux, aperture_fluxes)
+        features = prepare_input(feature_cfg, tce, time, flux, aperture_fluxes)
         for k, v in features.items():
             dataset[k].append([v])
     return tf.data.Dataset.from_tensor_slices(dataset)
+
+
+def find_checkpoints(base_dir: Path, nruns: Optional[int] = None) -> list[Path]:
+    if nruns is None:
+        nruns = len(list(base_dir.iterdir()))
+    return [next((base_dir / str(i)).iterdir()) for i in range(1, nruns + 1)]
+
+
+def predict(
+    model_dir: Path,
+    dataset: tf.data.Dataset,
+) -> np.ndarray:
+    model = tf.keras.models.load_model(model_dir)
+    return model.predict(dataset)
+
+
+def batch_predict(
+    checkpoints_dir: Path,
+    tces: pd.DataFrame,
+    get_lc: LCGetter,
+    mode: Literal["triage", "vetting"],
+    nruns: Optional[int] = None,
+    nprocs: int = 1,
+    **kwargs,
+):
+    model_dirs = find_checkpoints(checkpoints_dir, nruns)
+    first_model_dir = model_dirs[0]
+    with (first_model_dir / "config.json").open("r") as config_file:
+        config = json.load(config_file)
+    input_features_cfg = config["inputs"]["features"]
+    output_labels = config["inputs"]["label_columns"]
+    # Ensure all configured inputs/outputs are the same
+    for model_dir in model_dirs:
+        with (model_dir / "config.json").open("r") as cfg_file:
+            model_cfg = json.load(cfg_file)
+        if model_cfg["inputs"]["features"] != input_features_cfg:
+            raise ValueError(
+                f"Configured inputs in {model_dir} do not match first checkpoint."
+                f"\nFirst checkpoint:\n{input_features_cfg}"
+                f"\n{model_dir}:\n{model_cfg['inputs']['features']}"
+            )
+        if model_cfg["inputs"]["label_columns"] != output_labels:
+            raise ValueError(
+                f"Configured output labels in {model_dir} do not match first checkpoint."
+                f"\nFirst checkpoint:\n{output_labels}"
+                f"\n{model_dir}:\n{model_cfg['inputs']['label_columns']}"
+            )
+
+    dataset = build_dataset(input_features_cfg, tces, get_lc, mode)
+    predictions = [predict(model_dir, dataset) for model_dir in model_dirs]
+    prediction_dfs = [
+        pd.DataFrame(
+            pred,
+            index=pd.MultiIndex.from_product(
+                [tces["Astro ID"], [i]], names=["Astro ID", "model_no"]
+            ),
+        )
+        for i, pred in enumerate(predictions)
+    ]
+    return pd.concat(prediction_dfs)
