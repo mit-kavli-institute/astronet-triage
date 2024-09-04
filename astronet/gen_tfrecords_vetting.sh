e0a022ca02b03f91ccb1ca5581a807d543fb9bc4
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2024-09-04 11:09:18 -0400

    generate_input_records.py now takes argument "mode" instead of "vetting_features"

--- astronet/gen_tfrecords_vetting.sh
+++ astronet/gen_tfrecords_vetting.sh
@@ -4,11 +4,11 @@ set -e
 
 LCDIR=../mnt/tess/lc
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v02-tois_as_planets-train.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-v02-tois_as_planets-train --vetting_features=y --num_shards=5
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v02-tois_as_planets-train.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-v02-tois_as_planets-train --mode=vetting --num_shards=5
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v02-tois_as_planets-val.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-v02-tois_as_planets-val --vetting_features=y --num_shards=5
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v02-tois_as_planets-val.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-v02-tois_as_planets-val --mode=vetting --num_shards=5
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v02-tois_as_planets-test.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-v02-tois_as_planets-test --vetting_features=y --num_shards=5
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v02-tois_as_planets-test.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-v02-tois_as_planets-test --mode=vetting --num_shards=5
 
 
 
@@ -19,3 +19,4 @@ python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt
 # python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v7-toi-val.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-8-toi-val --vetting_features=y --num_shards=2
 
 # # python astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-vetting-v7-toi-test.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-vetting-8-toi-test --vetting_features=y --num_shards=2
+4
