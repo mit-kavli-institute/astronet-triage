b0bed6156ac78fb544c1aead9caf31ee3beef2c5
Chris Shallue <chris.shallue@gmail.com>
2025-03-14 16:02:54 -0400

    Remove unused imports

--- astronet/astro_cnn_model/input_ds.py
+++ astronet/astro_cnn_model/input_ds.py
@@ -18,9 +18,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl import logging
-import collections
-import six
 import tensorflow as tf
 
 
