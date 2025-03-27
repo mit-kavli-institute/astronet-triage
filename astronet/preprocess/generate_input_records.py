470f1e8c58ce67f7ce31605d3c2628ee5f63ff0a
Chris Shallue <chris.shallue@gmail.com>
2025-03-27 11:16:56 -0400

    Factor out example_util.py

--- astronet/preprocess/generate_input_records.py
+++ astronet/preprocess/generate_input_records.py
@@ -24,6 +24,7 @@ from absl import app, flags, logging
 from typing_extensions import Protocol
 
 from astronet.preprocess import preprocess
+from astronet.util.example_util import set_float_feature, set_int64_feature
 
 
 class LCGetter(Protocol):
@@ -57,30 +58,6 @@ flags.DEFINE_bool("training", True, "Whether to generate for training.")
 FLAGS = flags.FLAGS
 
 
-def _set_float_feature(ex, name, value):
-  """Sets the value of a float feature in a tensorflow.train.Example proto."""
-  assert name not in ex.features.feature, f"Duplicate feature: {name}"
-  if isinstance(value, np.ndarray):
-    value = value.reshape((-1,))
-  values = [float(v) for v in value]
-  if any(np.isnan(values)):
-    raise ValueError(f"NaNs in {name}")
-  ex.features.feature[name].float_list.value.extend(values)
-
-
-def _set_bytes_feature(ex, name, value):
-  """Sets the value of a bytes feature in a tensorflow.train.Example proto."""
-  assert name not in ex.features.feature, f"Duplicate feature: {name}"
-  ex.features.feature[name].bytes_list.value.extend(
-      [str(v).encode("latin-1") for v in value])
-
-
-def _set_int64_feature(ex, name, value):
-  """Sets the value of an int64 feature in a tensorflow.train.Example proto."""
-  assert name not in ex.features.feature, f"Duplicate feature: {name}"
-  ex.features.feature[name].int64_list.value.extend([int(v) for v in value])
-
-
 def _standard_views(ex, tic, time, flux, period, epoc, duration, bkspace,
                     aperture_fluxes):
   if bkspace is None:
@@ -98,22 +75,22 @@ def _standard_views(ex, tic, time, flux, period, epoc, duration, bkspace,
 
   view, std, mask, _, _ = preprocess.global_view(tic, time, flux, period)
   tr_mask, _, _, _, _ = preprocess.tr_mask_view(tic, time, tr_mask, period)
-  _set_float_feature(ex, f"global_view{tag}", view)
-  _set_float_feature(ex, f"global_std{tag}", std)
-  _set_float_feature(ex, f"global_mask{tag}", mask)
-  _set_float_feature(ex, f"global_transit_mask{tag}", tr_mask)
+  set_float_feature(ex, f"global_view{tag}", view)
+  set_float_feature(ex, f"global_std{tag}", std)
+  set_float_feature(ex, f"global_mask{tag}", mask)
+  set_float_feature(ex, f"global_transit_mask{tag}", tr_mask)
 
   view, std, mask, scale, depth = preprocess.local_view(tic, time, flux, period,
                                                         duration)
-  _set_float_feature(ex, f"local_view{tag}", view)
-  _set_float_feature(ex, f"local_std{tag}", std)
-  _set_float_feature(ex, f"local_mask{tag}", mask)
+  set_float_feature(ex, f"local_view{tag}", view)
+  set_float_feature(ex, f"local_std{tag}", std)
+  set_float_feature(ex, f"local_mask{tag}", mask)
   if scale is not None:
-    _set_float_feature(ex, f"local_scale{tag}", [scale])
-    _set_float_feature(ex, f"local_scale_present{tag}", [1.0])
+    set_float_feature(ex, f"local_scale{tag}", [scale])
+    set_float_feature(ex, f"local_scale_present{tag}", [1.0])
   else:
-    _set_float_feature(ex, f"local_scale{tag}", [0.0])
-    _set_float_feature(ex, f"local_scale_present{tag}", [0.0])
+    set_float_feature(ex, f"local_scale{tag}", [0.0])
+    set_float_feature(ex, f"local_scale_present{tag}", [0.0])
   for k, (t, f) in aperture_fluxes.items():
     t, f, m = preprocess.detrend_and_filter(tic, t, f, period, epoc, duration,
                                             bkspace)
@@ -121,38 +98,38 @@ def _standard_views(ex, tic, time, flux, period, epoc, duration, bkspace,
         t, f, m, period, epoc)
     view, std, _, _, _ = preprocess.local_view(
         tic, t, f, period, duration, scale=scale, depth=depth)
-    _set_float_feature(ex, f"local_aperture_{k}{tag}", view)
+    set_float_feature(ex, f"local_aperture_{k}{tag}", view)
 
   view, std, mask, _, _ = preprocess.local_view(
       tic, time[odds], flux[odds], period, duration, scale=scale, depth=depth)
-  _set_float_feature(ex, f"local_view_odd{tag}", view)
-  _set_float_feature(ex, f"local_std_odd{tag}", std)
-  _set_float_feature(ex, f"local_mask_odd{tag}", mask)
+  set_float_feature(ex, f"local_view_odd{tag}", view)
+  set_float_feature(ex, f"local_std_odd{tag}", std)
+  set_float_feature(ex, f"local_mask_odd{tag}", mask)
 
   view, std, mask, _, _ = preprocess.local_view(
       tic, time[evens], flux[evens], period, duration, scale=scale, depth=depth)
-  _set_float_feature(ex, f"local_view_even{tag}", view)
-  _set_float_feature(ex, f"local_std_even{tag}", std)
-  _set_float_feature(ex, f"local_mask_even{tag}", mask)
+  set_float_feature(ex, f"local_view_even{tag}", view)
+  set_float_feature(ex, f"local_std_even{tag}", std)
+  set_float_feature(ex, f"local_mask_even{tag}", mask)
 
   (_, _, _, sec_scale,
    _), t0 = preprocess.secondary_view(tic, time, flux, period, duration)
   (view, std, mask, scale, _), t0 = preprocess.secondary_view(
       tic, time, flux, period, duration, scale=scale, depth=depth)
-  _set_float_feature(ex, f"secondary_view{tag}", view)
-  _set_float_feature(ex, f"secondary_std{tag}", std)
-  _set_float_feature(ex, f"secondary_mask{tag}", mask)
-  _set_float_feature(ex, f"secondary_phase{tag}", [t0 / period])
+  set_float_feature(ex, f"secondary_view{tag}", view)
+  set_float_feature(ex, f"secondary_std{tag}", std)
+  set_float_feature(ex, f"secondary_mask{tag}", mask)
+  set_float_feature(ex, f"secondary_phase{tag}", [t0 / period])
   if sec_scale is not None:
-    _set_float_feature(ex, f"secondary_scale{tag}", [sec_scale])
-    _set_float_feature(ex, f"secondary_scale_present{tag}", [1.0])
+    set_float_feature(ex, f"secondary_scale{tag}", [sec_scale])
+    set_float_feature(ex, f"secondary_scale_present{tag}", [1.0])
   else:
-    _set_float_feature(ex, f"secondary_scale{tag}", [0.0])
-    _set_float_feature(ex, f"secondary_scale_present{tag}", [0.0])
+    set_float_feature(ex, f"secondary_scale{tag}", [0.0])
+    set_float_feature(ex, f"secondary_scale_present{tag}", [0.0])
 
   full_view = preprocess.sample_segments_view(tic, time, flux, fold_num, period,
                                               duration)
-  _set_float_feature(ex, f"sample_segments_view{tag}", full_view)
+  set_float_feature(ex, f"sample_segments_view{tag}", full_view)
 
   odd_view = preprocess.sample_segments_view(
       tic,
@@ -175,30 +152,30 @@ def _standard_views(ex, tic, time, flux, period, epoc, duration, bkspace,
       num_transits=4,
       local=True)
   full_view = np.concatenate([odd_view, even_view], axis=-1)
-  _set_float_feature(ex, f"sample_segments_local_view{tag}", full_view)
+  set_float_feature(ex, f"sample_segments_local_view{tag}", full_view)
 
   time, flux, fold_num, _ = preprocess.phase_fold_and_sort_light_curve(
       detrended_time, detrended_flux, transit_mask, period * 2,
       epoc - period / 2)
   view, std, mask, scale, _ = preprocess.global_view(tic, time, flux,
                                                      period * 2)
-  _set_float_feature(ex, f"global_view_double_period{tag}", view)
-  _set_float_feature(ex, f"global_view_double_period_std{tag}", std)
-  _set_float_feature(ex, f"global_view_double_period_mask{tag}", mask)
+  set_float_feature(ex, f"global_view_double_period{tag}", view)
+  set_float_feature(ex, f"global_view_double_period_std{tag}", std)
+  set_float_feature(ex, f"global_view_double_period_mask{tag}", mask)
 
   time, flux, fold_num, _ = preprocess.phase_fold_and_sort_light_curve(
       detrended_time, detrended_flux, transit_mask, period / 2, epoc)
   view, std, mask, scale, _ = preprocess.global_view(tic, time, flux,
                                                      period / 2)
-  _set_float_feature(ex, f"global_view_half_period{tag}", view)
-  _set_float_feature(ex, f"global_view_half_period_std{tag}", std)
-  _set_float_feature(ex, f"global_view_half_period_mask{tag}", mask)
+  set_float_feature(ex, f"global_view_half_period{tag}", view)
+  set_float_feature(ex, f"global_view_half_period_std{tag}", std)
+  set_float_feature(ex, f"global_view_half_period_mask{tag}", mask)
 
   view, std, mask, scale, _ = preprocess.local_view(tic, time, flux, period / 2,
                                                     duration)
-  _set_float_feature(ex, f"local_view_half_period{tag}", view)
-  _set_float_feature(ex, f"local_view_half_period_std{tag}", std)
-  _set_float_feature(ex, f"local_view_half_period_mask{tag}", mask)
+  set_float_feature(ex, f"local_view_half_period{tag}", view)
+  set_float_feature(ex, f"local_view_half_period_std{tag}", std)
+  set_float_feature(ex, f"local_view_half_period_mask{tag}", mask)
 
   return fold_num
 
@@ -221,63 +198,63 @@ def _process_tce(tce, get_lightcurve: LCGetter, mode: AstronetMode,
     fold_num = _standard_views(ex, tce["TIC ID"], time, flux, tce.Per, tce.Epoc,
                                tce.Dur, bkspace, apertures)
 
-  _set_int64_feature(ex, "astro_id", [tce["Astro ID"]])
+  set_int64_feature(ex, "astro_id", [tce["Astro ID"]])
 
   if training:
     if mode == "vetting":
-      _set_int64_feature(ex, "disp_e", [tce["disp_e"]])
-      _set_int64_feature(ex, "disp_p", [tce["disp_p"]])
-      _set_int64_feature(ex, "disp_n", [tce["disp_n"]])
-      _set_int64_feature(ex, "disp_b", [tce["disp_b"]])
-      _set_int64_feature(ex, "disp_t", [tce["disp_t"]])
-      _set_int64_feature(ex, "disp_u", [tce["disp_u"]])
-      _set_int64_feature(ex, "disp_j", [tce["disp_j"]])
+      set_int64_feature(ex, "disp_e", [tce["disp_e"]])
+      set_int64_feature(ex, "disp_p", [tce["disp_p"]])
+      set_int64_feature(ex, "disp_n", [tce["disp_n"]])
+      set_int64_feature(ex, "disp_b", [tce["disp_b"]])
+      set_int64_feature(ex, "disp_t", [tce["disp_t"]])
+      set_int64_feature(ex, "disp_u", [tce["disp_u"]])
+      set_int64_feature(ex, "disp_j", [tce["disp_j"]])
     elif mode == "triage":
-      _set_int64_feature(ex, "disp_E", [tce["disp_E"]])
-      _set_int64_feature(ex, "disp_N", [tce["disp_N"]])
-      _set_int64_feature(ex, "disp_J", [tce["disp_J"]])
-      _set_int64_feature(ex, "disp_S", [tce["disp_S"]])
-      _set_int64_feature(ex, "disp_B", [tce["disp_B"]])
+      set_int64_feature(ex, "disp_E", [tce["disp_E"]])
+      set_int64_feature(ex, "disp_N", [tce["disp_N"]])
+      set_int64_feature(ex, "disp_J", [tce["disp_J"]])
+      set_int64_feature(ex, "disp_S", [tce["disp_S"]])
+      set_int64_feature(ex, "disp_B", [tce["disp_B"]])
     else:
       raise ValueError(f"Mode '{mode}' not supported.")
 
   assert not np.isnan(tce.Per)
-  _set_float_feature(ex, "Period", [tce.Per])
+  set_float_feature(ex, "Period", [tce.Per])
 
   assert not np.isnan(tce.Dur)
-  _set_float_feature(ex, "Duration", [tce.Dur])
+  set_float_feature(ex, "Duration", [tce.Dur])
 
   assert not np.isnan(tce.Depth)
-  _set_float_feature(ex, "Transit_Depth", [tce.Depth])
+  set_float_feature(ex, "Transit_Depth", [tce.Depth])
 
   assert not np.isnan(tce.Tmag)
-  _set_float_feature(ex, "Tmag", [tce.Tmag])
+  set_float_feature(ex, "Tmag", [tce.Tmag])
 
-  # _set_float_feature(ex, "centroid_dist", [tce.centroid_dist])
+  # set_float_feature(ex, "centroid_dist", [tce.centroid_dist])
 
   if np.isnan(tce.SMass):
-    _set_float_feature(ex, "star_mass", [0])
-    _set_float_feature(ex, "star_mass_present", [0])
+    set_float_feature(ex, "star_mass", [0])
+    set_float_feature(ex, "star_mass_present", [0])
   else:
-    _set_float_feature(ex, "star_mass", [tce.SMass])
-    _set_float_feature(ex, "star_mass_present", [1])
+    set_float_feature(ex, "star_mass", [tce.SMass])
+    set_float_feature(ex, "star_mass_present", [1])
 
   if np.isnan(tce.SRad):
-    _set_float_feature(ex, "star_rad", [0])
-    _set_float_feature(ex, "star_rad_present", [0])
+    set_float_feature(ex, "star_rad", [0])
+    set_float_feature(ex, "star_rad_present", [0])
   else:
-    _set_float_feature(ex, "star_rad", [tce.SRad])
-    _set_float_feature(ex, "star_rad_present", [1])
+    set_float_feature(ex, "star_rad", [tce.SRad])
+    set_float_feature(ex, "star_rad_present", [1])
 
   if np.isnan(tce.SRadEst):
-    _set_float_feature(ex, "star_rad_est", [0])
-    _set_float_feature(ex, "star_rad_est_present", [0])
+    set_float_feature(ex, "star_rad_est", [0])
+    set_float_feature(ex, "star_rad_est_present", [0])
   else:
-    _set_float_feature(ex, "star_rad_est", [tce.SRadEst])
-    _set_float_feature(ex, "star_rad_est_present", [1])
+    set_float_feature(ex, "star_rad_est", [tce.SRadEst])
+    set_float_feature(ex, "star_rad_est_present", [1])
 
-  _set_float_feature(ex, "n_folds", [len(set(fold_num))])
-  _set_float_feature(ex, "n_points", [len(fold_num)])
+  set_float_feature(ex, "n_folds", [len(set(fold_num))])
+  set_float_feature(ex, "n_points", [len(fold_num)])
 
   return ex
 
