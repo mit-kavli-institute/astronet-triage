c252de90245c537a60a619194bfdb920cf597478
Chris Shallue <chris.shallue@gmail.com>
2025-03-28 16:50:17 -0400

    Refactor and simplify AstroCNNModelVetting

--- astronet/astro_cnn_model/astro_cnn_model_vetting.py
+++ astronet/astro_cnn_model/astro_cnn_model_vetting.py
@@ -30,48 +30,18 @@ class AstroCNNModelVetting(tf.keras.Model):
     self.triage_model = triage_model
     if not config.vetting_hparams.use_preds_layer:
       self.triage_model.make_embeds_only()
-    self.ts_blocks = base.create_ts_blocks(config.hparams)
+    self.ts_blocks = base.create_ts_blocks(config.vetting_hparams)
     self.final = base.build_final_fc_layers(config.inputs,
                                             config.vetting_hparams)
 
   def call(self, inputs, training=None):
-
-    def is_vetting_input(k):
-      if k.endswith('_present'):
-        k = k[:-len('_present')]
-      # The dataset makes them lowercase. We should change things to lowercase
-      # throughout.
-      if k not in self.config.inputs.features:
-        k, = tuple(
-            ck for ck in self.config.inputs.features.keys() if ck.lower() == k)
-      return self.config.inputs.features[k].get('vetting_only', False)
-
-    triage_inputs = {k: v for k, v in inputs.items() if not is_vetting_input(k)}
-    vetting_inputs = {k: v for k, v in inputs.items() if k in self.ts_blocks}
-
-    triage_embedding = self.triage_model(triage_inputs, training=training)
-
-    # TODO(cshallue): use base.unpack_inputs() once I confirm what goes where.
-    ts_inputs = {}
-    aux_inputs = {}
-    for k, v in vetting_inputs.items():
-      if k in self.config.vetting_hparams.time_series_hidden:
-        c = self.config.vetting_hparams.time_series_hidden[k]
-        chans = [v]
-        for extra in getattr(c, 'extra_channels', []):
-          chans.append(inputs[extra])
-        if getattr(c, 'multichannel', False):
-          ts_inputs[k] = tf.concat(chans, axis=-1)
-        else:
-          ts_inputs[k] = tf.stack(chans, axis=-1)
-      elif k in self.config.hparams.aux_inputs:
-        aux_inputs[k] = v
-
-    y = [triage_embedding]
+    ts_inputs, aux_inputs = base.unpack_inputs(inputs, self.config.hparams)
+    y = []
     for k in sorted(ts_inputs.keys()):
       v = ts_inputs[k]
       y.append(base.apply_block(self.ts_blocks[k], v, training))
     y.extend([aux_inputs[k] for k in sorted(aux_inputs.keys())])
+    y.append(self.triage_model(inputs, training=training))  # Triage embedding.
     y = base.apply_block(self.final, y, training)
 
     return y
