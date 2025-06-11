701534faf622fda1f49e1ebb0f52acdf9e0369a1
David Dimond <dpdimondjr@gmail.com>
2025-06-11 11:12:34 -0400

    dashboard fixes

--- astronet/gen_tfrecords_vetting.sh
+++ astronet/gen_tfrecords_vetting.sh
@@ -9,7 +9,7 @@ NAME=vetting-sector86-all
 
 #python preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-val.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-${NAME}-val --mode=vetting --num_shards=5
 
-python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/pdo/users/dimond/mnt/tess/astronet/tces-sector86-all2.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/users/dimond/mnt/tess/astronet/tfrecords-${NAME}-test --mode=vetting --num_shards=25
+python astronet/preprocess/generate_input_records.py --input_tce_csv_file=/pdo/users/dimond/mnt/tess/astronet/tces-sector86-all2.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/users/dimond/mnt/tess/astronet/tfrecords-${NAME}-test --mode=vetting
 
 
 
