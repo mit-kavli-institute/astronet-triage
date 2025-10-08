d236259f334958a0c1f4f3445b2ccb0eb2208981
pablomerc <pablomer@mit.edu>
2025-10-08 10:02:54 -0400

    WIP

--- astronet/gen_tfrecords_vetting_2025tois_2_copy_train.sh
+++ astronet/gen_tfrecords_vetting_2025tois_2_copy_train.sh
@@ -5,12 +5,17 @@ set -e
 # LCDIR=../mnt/tess/lc_vetting_and_triage_symbolic_links
 # LCDIR=/pdo/users/pablomer/mnt/tess/new_fit_files_combined
 # LCDIR=../mnt/tess/lc_vetting_and_triage_symbolic_links
-LCDIR=../mnt/tess/april2025_dataset_fits_files/
+LCDIR=/pdo/astronet-data/data/fits/sector-87-p-v2
+# CSVDIR=/pdo/users/pablomer/mnt/tess/debug-h5fits/tois_sector_85_to_87_all_renamed_withfilenames_renamed.csv
 
+CSVDIR=/pdo/astronet-data/data/properties/tces-sector87-p.csv
 # NAME=vetting-v01-tois-triageJs-nocentroid-april2025dataset
-NAME=vetting-v01-tois-triageJs-nocentroid-april2025
+# NAME=vetting-v01-tois-triageJs-nocentroid-april2025
+NAME=sector87
 
-/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-train.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-${NAME}-train --mode=vetting --num_shards=5
+OUTPUTDIR=/pdo/astronet-data/data/tfrecords/sector-87-p-v4
+
+/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records.py --input_tce_csv_file=${CSVDIR} --tess_data_dir=${LCDIR} --output_dir=${OUTPUTDIR} --mode=vetting --num_shards=20 --training=false
 
 # /pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-val.csv --tess_data_dir=${LCDIR} --output_dir=../mnt/tess/astronet/tfrecords-${NAME}-val --mode=vetting --num_shards=5
 
