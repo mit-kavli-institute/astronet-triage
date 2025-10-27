c1a70cd089736f0aff441fb8caea2d8427385444
pablomerc <pablomer@mit.edu>
2025-10-27 17:43:10 -0400

    last training run

--- astronet/ensemble_train_vetting_2025.sh
+++ astronet/ensemble_train_vetting_2025.sh
@@ -6,18 +6,18 @@ DATE=$(date +%Y%m%d)
 CONFIG_NAME=pablomer
 #CONFIG_OVERRIDES="inputs.random_reverse_time_series=true"
 # CONFIG_OVERRIDES="train_steps=1000"
-CONFIG_OVERRIDES="train_steps=1000,init_from_pretrained_model=false"
+CONFIG_OVERRIDES="train_steps=2000,init_from_pretrained_model=true"
 ENSEMBLE_NAME=$CONFIG_NAME
 
 
 # PRETRAIN_MODEL_DIR=/pdo/users/cshallue/astronet/models/triage/20250420/cshallue-h5/AstroCNNModel_cshallue_20250420_174804
-# PRETRAIN_MODEL_DIR=/pdo/users/pablomer/mnt/tess/models/triage/20250520/pablomer-h5/AstroCNNModel_pablomer_20250520_181651
+PRETRAIN_MODEL_DIR=/pdo/users/pablomer/mnt/tess/models/triage/20250520/pablomer-h5/AstroCNNModel_pablomer_20250520_181651
 
 # CODE_DIR=/pdo/users/cshallue/git/astronet
 CODE_DIR=/pdo/users/pablomer/Astronet-Triage
 # DATA_DIR=/pdo/users/pablomer/mnt/tess/astronet/
 DATA_DIR=/pdo/astronet-data/data/tfrecords/oct2025_original/
-
+# DATA_DIR=/pdo/astronet-data/data/tfrecords/oct2025_cadencebin/
 
 # TFRECORD_PREFIX=tfrecords-vetting-v01-tois-triageJs-nocentroid-newTOIs2025-v3-noreshufle
 
@@ -25,7 +25,7 @@ TFRECORD_PREFIX=tfrecords-vetting-v01-tois-triageJs-nocentroid-april2025
 
 
 # OUTPUT_DIR=/pdo/users/pablomer/mnt/tess/models/vetting/$DATE/$ENSEMBLE_NAME/
-OUTPUT_DIR=/pdo/astronet-data/models/vetting/experimental/pablomer/vanilla/$DATE/$ENSEMBLE_NAME/
+OUTPUT_DIR=/pdo/astronet-data/models/vetting/experimental/pablomer/original/$DATE/$ENSEMBLE_NAME/
 
 
 # CONFIG_OVERRIDES="init_from_pretrained_model=true,\
