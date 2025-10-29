db21b8d727827291cba784f1bcf88f21b1d04acb
pablomerc <pablomer@mit.edu>
2025-10-29 14:40:56 -0400

    increasing number of shards to debug;

--- astronet/gen_tfrecords_vetting_2025tois_2.sh
+++ astronet/gen_tfrecords_vetting_2025tois_2.sh
@@ -13,8 +13,11 @@ LCDIR=/pdo/astronet-data/data/fits/oct2025_dataset_all_fits_files_30min_bin/
 # NAME=vetting-v01-tois-triageJs-nocentroid-april2025dataset
 NAME=vetting-v01-tois-triageJs-nocentroid-april2025
 
-/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records_2.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-train.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/astronet-data/data/tfrecords/oct2025_30minbin/tfrecords-${NAME}-train --mode=vetting --num_shards=50
+# Redirect all output (stdout + stderr) to log.txt
+exec > >(tee "generate_records_${NAME}_2.log") 2>&1
 
-/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records_2.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-val.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/astronet-data/data/tfrecords/oct2025_30minbin/tfrecords-${NAME}-val --mode=vetting --num_shards=50
+/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records_2.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-train.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/astronet-data/data/tfrecords/oct2025_30minbin_redo1/tfrecords-${NAME}-train --mode=vetting --num_shards=6000
 
-/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records_2.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-test.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/astronet-data/data/tfrecords/oct2025_30minbin/tfrecords-${NAME}-test --mode=vetting --num_shards=50
+/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records_2.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-val.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/astronet-data/data/tfrecords/oct2025_30minbin_redo1/tfrecords-${NAME}-val --mode=vetting --num_shards=600
+
+/pdo/users/pablomer/miniconda3/envs/daniel_env_cloned_v2/bin/python3 astronet/preprocess/generate_input_records_2.py --input_tce_csv_file=../mnt/tess/astronet/tces-${NAME}-test.csv --tess_data_dir=${LCDIR} --output_dir=/pdo/astronet-data/data/tfrecords/oct2025_30minbin_redo1/tfrecords-${NAME}-test --mode=vetting --num_shards=600
