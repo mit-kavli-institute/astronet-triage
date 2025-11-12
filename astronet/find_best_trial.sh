59911d1732b58ba982afb0a8427a9e22372e263c
pablomerc <pablomer@mit.edu>
2025-11-11 21:33:39 -0500

    added a comment with most recent use case

--- astronet/find_best_trial.sh
+++ astronet/find_best_trial.sh
@@ -10,3 +10,8 @@ python astronet/tuning/find_best_trial.py "$@"
 # ./astronet/find_best_trial.sh \
 #      --study_dir=/pdo/users/cshallue/astronet/models/triage-tune/20250915/decay-adam-alpha0p01 \
 #      --metric=val_loss
+
+
+# ./astronet/find_best_trial.sh \
+#      --study_dir=/pdo/astronet-data/models/vetting/experimental/pablomer/tuning/20251110/cosinedecay-adam \
+#      --metric=val_average_precision
